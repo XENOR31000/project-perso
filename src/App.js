db6e0d9dd9a4d15280f373eeedf72586c171507d
XENOR31000 <axel.ibanez2@lycee-pardailhan.fr>
2023-01-07 15:59:51 +0100

    test

--- src/App.js
+++ src/App.js
@@ -6,16 +6,21 @@ import { useState } from "react";
 import Profile from "./pages/Profile";
 
 function App() {
+    // Initialise l'état "user" à "null"
     const [user, setUser] = useState(null);
   
     return (
       <div className="App">
+        {/* Utilise le composant "BrowserRouter" de "react-router-dom" pour gérer la navigation entre les routes */}
         <BrowserRouter>
+          {/* Fournit les valeurs "user" et "setUser" au contexte "UserContext" */}
           <UserContext.Provider value={{ user, setUser }}>
+            {/* Définit les routes de l'application et spécifie les composants à afficher pour chaque route */}
             <Routes>
               <Route path="/" element={<Home />}></Route>
               <Route path="/connexion" element={<Connexion />}></Route>
               <Route path="/profile" element={<Profile />}></Route>
+              {/* La dernière route "*" correspond à toutes les autres routes, et affiche le composant "Home" */}
               <Route path="*" element={<Home />}></Route>
             </Routes>
           </UserContext.Provider>
@@ -25,4 +30,3 @@ function App() {
   }
   
   export default App;
-  
