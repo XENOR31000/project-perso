f17ece7d372d9e4168c811a86bf0131991fa36e3
XENOR31000 <axel.ibanez2@lycee-pardailhan.fr>
2023-01-09 20:29:59 +0100

    test

--- src/components/Home/BandeauTopAccueil.js
+++ src/components/Home/BandeauTopAccueil.js
@@ -5,9 +5,11 @@ const BandeauTopAccueil = () => {
     <div>
   
       <div className="bandeauAccueilComponent">
-        <div className="titre">
+      <div className="titre">
         <h1 className="titre-bandeau" >GARAGE MERCEDES</h1>
         </div>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/iSnP5c997Uk?&autoplay=1&mute=1&playsinline=1&controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        
       </div>
     </div>
   );
